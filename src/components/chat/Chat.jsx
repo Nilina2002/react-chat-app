139c9d9b96d30d32ae007441edbbdf7a4fca29a7
Nilina Nilaksha <nilina610@gmail.com>
2024-12-29 00:13:40 +0530

    overall - ui

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -6,6 +6,12 @@ const Chat = () => {
   const [open, setOpen] = useState(false);
   const [text, setText] = useState("");
 
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
   const handleEmoji = (e) => {
     setText((prev) => prev + e.emoji);
     setOpen(false);
@@ -26,7 +32,82 @@ const Chat = () => {
           <img src="./info.png" alt="" />
         </div>
       </div>
-      <div className="center"></div>
+      <div className="center">
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img
+              src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg"
+              alt=""
+            />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+              doloremque voluptatem debitis at nisi beatae vero cum, fugit
+              explicabo? Natus libero saepe aliquid quisquam quaerat blanditiis
+              eos laboriosam excepturi ad.
+            </p>
+            <span>1min ago</span>
+          </div>
+        </div>
+        <div ref={endRef}></div>
+      </div>
       <div className="bottom">
         <div className="icons">
           <label htmlFor="file">
--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,8 +1,92 @@
 import React from "react";
 import "./details.css";
 
-const details = () => {
-  return <div className="details">details</div>;
+const Detail = () => {
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Charitha</h2>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block</button>
+        <button className="logout">Log Out</button>
+      </div>
+    </div>
+  );
 };
 
-export default details;
+export default Detail;
